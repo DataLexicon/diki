12dc348919030dc51b72a7250d1b6fc4ddc2a117
judahwon <hwondaa@gmail.com>
2024-09-29 16:32:12 +0900

    feat: 테마 및 헤더 구현

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   darkMode: ['class'],
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{ts,tsx,mdx}'],
   theme: {
     extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
     },
   },
   plugins: [],
